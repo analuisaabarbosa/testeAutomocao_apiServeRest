bad7944bb23b42b88aa994a6a77b9a122531353f
Analu <analuisaaugustob@gmail.com>
2024-11-19 11:35:44 -0300

    Adicionando configurações do Report Portal para gerenciar os testes e dashboards

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
 const { defineConfig } = require("cypress");
+const registerReportPortalPlugin = require("@reportportal/agent-js-cypress/lib/plugin");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return registerReportPortalPlugin(on, config);
     },
     baseUrl: "http://localhost:3000/",
+    reporter: "@reportportal/agent-js-cypress",
+    reporterOptions: {
+      endpoint: "https://demo.reportportal.io/api/v1",
+      apiKey:
+        "EBAC-Test_lekr5RmZSYSmSqDMtadxNy7LpOHXoJsiYTczEsQQBTKcSLdjG4iJWfxFVkbH4QuF",
+      project: "analuisaabarbosa_personal",
+      launch: "Testes de API com Cypress",
+      description: "Testando funcionalidades da API ServeRest",
+    },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,43 +1,47 @@
-Cypress.Commands.add('token', (email, password) => {
-    cy.request({
-        method: 'POST',
-        url: 'login',
-        body: {
-            "email": email,
-            "password": password 
-        }
-    }).then((response) => {
-        expect(response.status).to.equal(200)
-        return response.body.authorization
-    })
- })
+require("@reportportal/agent-js-cypress/lib/commands/reportPortalCommands");
 
- Cypress.Commands.add('addProduct' , (token, products, price, description, quantity) =>{
-    cy.request({
-        method: 'POST', 
-        url: 'produtos',
-        headers: {authorization: token}, 
-        body: {
-            "nome": products,
-            "preco": price,
-            "descricao": description,
-            "quantidade": quantity
-          }, 
-          failOnStatusCode: false
-    })
- })
+Cypress.Commands.add("token", (email, password) => {
+  cy.request({
+    method: "POST",
+    url: "login",
+    body: {
+      email: email,
+      password: password,
+    },
+  }).then((response) => {
+    expect(response.status).to.equal(200);
+    return response.body.authorization;
+  });
+});
 
- Cypress.Commands.add('addUser', (name, email, password) => {
+Cypress.Commands.add(
+  "addProduct",
+  (token, products, price, description, quantity) => {
     cy.request({
-        method: 'POST',
-        url: 'usuarios',
-        body: {
-            "nome": name,
-            "email": email,
-            "password": password,
-            "administrador": 'true'
-        },
-        failOnStatusCode: false
-    })
- })
+      method: "POST",
+      url: "produtos",
+      headers: { authorization: token },
+      body: {
+        nome: products,
+        preco: price,
+        descricao: description,
+        quantidade: quantity,
+      },
+      failOnStatusCode: false,
+    });
+  }
+);
 
+Cypress.Commands.add("addUser", (name, email, password) => {
+  cy.request({
+    method: "POST",
+    url: "usuarios",
+    body: {
+      nome: name,
+      email: email,
+      password: password,
+      administrador: "true",
+    },
+    failOnStatusCode: false,
+  });
+});
